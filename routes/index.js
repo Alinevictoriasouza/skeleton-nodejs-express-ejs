adc4c0a83e70e088cf2c76ab044e59282cbc50cf
Aline Souza <86006464+Alinevictoriasouza@users.noreply.github.com>
2022-03-09 20:54:22 +0000

    A rota foi modificada

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const Autor = require("../models/autor");
 router.get('/', async function(req, res, next) {
   const autores = await Autor.selecionar();
   res.json(autores.rows);
-});
+}) ;
 
 module.exports = router;
